785b83a66358a2d38d3c3ef9062963474191f232
t-kabu <t.kabu.03@gmail.com>
2022-10-16 17:28:44 +0900

    lesson12の実装

--- lessons/lesson12/index.test.ts
+++ lessons/lesson12/index.test.ts
@@ -1,18 +1,16 @@
 import {
   convertToBinaryNumber,
   convertToBinaryNumberWithoutParseIntAndToString,
-} from ".";
+} from '.'
 
-describe("Lesson 12", () => {
-  test.skip("number型の整数が2進数の文字列に変換されること", () => {
-    expect(convertToBinaryNumber(10)).toBe("1010");
-    expect(convertToBinaryNumber(100)).toBe("1100100");
-  });
+describe('Lesson 12', () => {
+  test.skip('number型の整数が2進数の文字列に変換されること', () => {
+    expect(convertToBinaryNumber(10)).toBe('1010')
+    expect(convertToBinaryNumber(100)).toBe('1100100')
+  })
 
-  test.skip("number型の整数が2進数の文字列に変換されること", () => {
-    expect(convertToBinaryNumberWithoutParseIntAndToString(10)).toBe("1010");
-    expect(convertToBinaryNumberWithoutParseIntAndToString(100)).toBe(
-      "1100100"
-    );
-  });
-});
+  test('number型の整数が2進数の文字列に変換されること', () => {
+    expect(convertToBinaryNumberWithoutParseIntAndToString(10)).toBe('1010')
+    expect(convertToBinaryNumberWithoutParseIntAndToString(100)).toBe('1100100')
+  })
+})
--- lessons/lesson12/index.ts
+++ lessons/lesson12/index.ts
@@ -1,10 +1,17 @@
 export function convertToBinaryNumber(decimalNumber: number) {
-  return parseInt(decimalNumber.toString(), 10).toString(2);
+  return parseInt(decimalNumber.toString(), 10).toString(2).toString()
 }
 
 export function convertToBinaryNumberWithoutParseIntAndToString(
-  decimalNumber: number
+  decimalNumber: number,
 ) {
   // 2進数は、2のべき乗を足し合わせて表すことができます。
   // フローチャートなどを作成し、while文やfor文、文字列の足し算などで一つずつ実装していきましょう。
+  let result = ''
+
+  while (decimalNumber > 0) {
+    result = String(decimalNumber % 2) + result
+    decimalNumber = Math.floor(decimalNumber / 2)
+  }
+  return result
 }
